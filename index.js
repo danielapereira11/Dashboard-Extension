c7352751fbe38f589f652b1481bccfeff3e903a5
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2022-01-14 21:45:37 +0000

    Added an alternative image background if promise is rejected

--- index.js
+++ index.js
@@ -9,4 +9,10 @@ fetch(unsplashApiUrl)
     //   ADDING RANDOM BACKGROUND IMAGE FROM UNSPLASH API
     bodyEl.style.backgroundImage = `url("${data.urls.full}")`;
     imageAuthorEl.innerText = `Author: ${data.user.name}`;
+  })
+  // ADDING AN ALTERNATIVE IMAGE IF THERE IS AN ERROR GETTING THE IMAGE FROM THE API - IF THE PROMISE IS REJECTED
+  .catch((err) => {
+    bodyEl.style.backgroundImage = `url("https://images.unsplash.com/photo-1484821582734-6c6c9f99a672?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NDIxOTY1Nzk&ixlib=rb-1.2.1&q=85")`;
+    imageAuthorEl.innerText = "Author: Ishan @seefromthesky";
+    console.log(err);
   });
