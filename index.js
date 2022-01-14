4a7832f9e91345a8e106f84e769302934e92e739
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2022-01-14 19:25:03 +0000

    Added the background image author to page

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const bodyEl = document.querySelector("body");
+const imageAuthorEl = document.getElementById("image-author");
 const unsplashApiUrl =
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature";
 
 fetch(unsplashApiUrl)
   .then((resp) => resp.json())
-  .then(
-    (data) =>
-      //   ADDING RANDOM BACKGROUND IMAGE FROM UNSPLASH API
-      (bodyEl.style.backgroundImage = `url("${data.urls.full}")`)
-  );
+  .then((data) => {
+    //   ADDING RANDOM BACKGROUND IMAGE FROM UNSPLASH API
+    bodyEl.style.backgroundImage = `url("${data.urls.full}")`;
+    imageAuthorEl.innerText = `${data.user.name}`;
+  });
